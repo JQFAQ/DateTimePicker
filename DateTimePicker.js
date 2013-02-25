9872c0bbf0b3b5cd4439848e138f23a8dde0128e
JQFAQ <marketing@jqfaq.com>
2013-02-25 19:05:25 +0530

    Create DateTimePicker.js

---
+++ DateTimePicker.js
@@ -0,0 +1,46 @@
+(function ($) {
+    //var RQStyle = RadiantQ.Gantt.DefaultStyles;
+    $.widget("radiantq.DateTimePicker", {
+        currentUniqueRowSuffix: 0,
+        options: {
+            setdate:null
+        },
+        _create: function () {
+            //declarations
+            var self = this;
+            var year = 0, month = 0, day = 0, hour = 0, minute = 0;
+            var date = 0;
+            $('<input id="time" class="datetimepicker" style="float:left;"/>').insertAfter(this.element);
+            $('.datetimepicker').wrapAll('<div class="datetimepicker"></div>');
+            var $time = $("#time");
+            this.element.datepicker({
+                onSelect: function () {
+                    date = $(this).datepicker('getDate');
+                    year = date.getFullYear();
+                    month = date.getMonth();
+                    day = date.getDate();
+                    hour = date.getHours();
+                    minute = date.getMinutes();
+                    $time.val(hour + ":" + minute + " AM");
+                    $time.timespinner({
+                        start: new Date(date)
+                    });
+                }
+            });
+            $time.val("00" + ":" + "00" + " AM");
+            $time.timespinner({
+                start: new Date(+year, +month, +day, +hour, +minute, 0, 0)
+            });
+        },
+        GetDateAndTime: function () {
+            var time = $("#time").timespinner("getTime");
+            var date = this.element.val();
+            var dateandtime = date + " " + time;
+            return dateandtime;
+        },
+        setDate: function (value) {
+            this.element.datepicker("setDate", new Date(value));
+            $("#time").timespinner("setTime", new Date(value));
+        }
+    });
+})(jQuery);
