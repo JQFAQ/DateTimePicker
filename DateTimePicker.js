d07da322b5b5db1f8009d324445ba6bc5d80e133
JQFAQ <marketing@jqfaq.com>
2013-02-27 20:12:36 +0530

    Update DateTimePicker.js

--- DateTimePicker.js
+++ DateTimePicker.js
@@ -38,7 +38,7 @@
             var input=$(widget).find('input');
             input.width(($(widget).width()-25) + 'px');
             input.css('margin', 0 + 'px');
-            input.css('font-size', 13+'px');
+            input.css('font-size', 14+'px');
         },
         GetDateAndTime: function () {
             var time = $("#time").timespinner("getTime");
